38159acbdb861146925e73b06e103d4c183523ef
Ronnie Atuhaire <66623378+Ronlin1@users.noreply.github.com>
2024-06-21 10:01:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -124,5 +124,6 @@ def get_product_info(product_id):
         print(e)
         return jsonify({'error': str(e)}), 500
 
+print(product_info_storage)
 # if __name__ == '__main__':
 #     app.run(debug=True, host='0.0.0.0', port=5000)
